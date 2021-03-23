99a1b9fca119d9ed905606d3c43fc31084e97161
dani <daniimanzano1199@gmail.com>
2021-03-23 17:13:13 +0100

    login con autenticacion

--- src/main/java/org/proyecto/retroshare/controller/UserRestController.java
+++ src/main/java/org/proyecto/retroshare/controller/UserRestController.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.proyecto.retroshare.domain.Role;
 import org.proyecto.retroshare.domain.User;
 import org.proyecto.retroshare.repositories.RoleRepository;
@@ -13,10 +15,11 @@ import org.proyecto.retroshare.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,19 +30,29 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.sun.istack.NotNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.proyecto.retroshare.config.JwtTokenProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/retroshare/")
 public class UserRestController {
 
+	private static Logger log = LoggerFactory.getLogger(UserRestController.class);
 	// ENLACE BBDD
 	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
 	private RoleRepository roleRepository;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenProvider tokenProvider;
 
 	// CONSULTA DE TODOS LOS USUARIOS
 	@GetMapping(value = "/all")
@@ -123,6 +136,31 @@ public class UserRestController {
 		}
 
 		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<String> authenticate(@RequestBody User user) {
+		log.info("UserResourceImpl : authenticate");
+		JSONObject jsonObject = new JSONObject();
+		try {
+			Authentication authentication = authenticationManager
+					.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+			if (authentication.isAuthenticated()) {
+				String email = user.getEmail();
+				jsonObject.put("name", authentication.getName());
+				jsonObject.put("authorities", authentication.getAuthorities());
+				jsonObject.put("token", tokenProvider.createToken(email, userRepository.findByEmail(email).getRole()));
+				return new ResponseEntity<String>(jsonObject.toString(), HttpStatus.OK);
+			}
+		} catch (JSONException e) {
+			try {
+				jsonObject.put("exception", e.getMessage());
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+			}
+			return new ResponseEntity<String>(jsonObject.toString(), HttpStatus.UNAUTHORIZED);
+		}
+		return null;
 	}
 
 }
