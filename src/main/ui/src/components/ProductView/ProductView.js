ea09a1b0e5ed71661ced21448797cf7b72313998
luciahernand <luciluhzm@gmail.com>
2021-03-26 12:47:17 +0100

    Product view Completo + search

---
+++ src/main/ui/src/components/ProductView/ProductView.js
@@ -0,0 +1,67 @@
+import { Button, Container, Grid, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { render } from '@testing-library/react';
+import * as React from 'react'
+import './StyleProductView.css'
+
+const createMarkup=(text)=>{
+  return{__html: text};
+}
+
+const ProductView = (addProduct) => {
+    const [product, setProduct] = React.useState([]);
+   
+  React.useEffect(() => {
+
+    const id=window.location.pathname.split("/");
+    obtenerDatos(id[2]);
+
+  }, []);
+
+  const obtenerDatos = async (id) => {
+  
+    const data = await fetch( `http://localhost:8080/retroshare/pr/${id}`);
+    const product= await data.json();
+    setProduct(product);
+   
+  };
+ console.log(product.name)
+
+      return(
+        
+        <Container className="product-view">
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={8} className="image-wrapper">
+              <img
+
+              // onLoad={()=>{
+                // setLoading(false);
+                //}}
+                src={product.imgagen}
+                alt={product.name}
+              />
+            
+            
+            </Grid>
+            <Grid item xs={12} md={4} className="text">
+              <Typography variant="h2">{product.name}</Typography>
+              <Typography variant="p" dangerouslySetInnerHTML={createMarkup(product.description)}></Typography>
+              <Typography variant="h3">Price:{product.price}€</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                size="large"
+                className="custom-button"
+                onclick={() =>{
+                 // addProduct(product.id)
+                }}
+              >
+                <ShoppingCart/>Add Product
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
+      );
+ 
+}
+export default ProductView;
---
+++ src/main/ui/src/components/Search/AComplete.js
@@ -0,0 +1,110 @@
+import React, { Component , useState} from 'react'
+import TextField from '@material-ui/core/TextField';  
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import axios from 'axios';  
+import {  fade, makeStyles, withStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        "&:hover": {
+         visibility: "hidden",
+        },
+      },search: {
+        
+        position: "relative",
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        "&:hover": {
+          backgroundColor: fade(theme.palette.common.white, 0.35),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+          marginLeft: theme.spacing(3),
+          width: "auto",
+        },
+      },
+     
+    inputRoot: {
+        color: "black",
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+          width: "20ch",
+        },
+      },
+}))
+
+export class AComplete extends Component {  
+        constructor(props) {  
+                super(props)  
+                
+                this.state = {  
+                        ProductData: []  
+                }  
+        }  
+       
+        componentDidMount() {  
+
+                axios.get('http://localhost:8080/retroshare/pr-all').then(response => {  
+                        console.log(response.data);  
+                        this.setState({  
+                                ProductData: response.data  
+                        });  
+                });  
+        }  
+        
+        render() {  
+            const { classes } = this.props;
+           
+                return (  
+                    <div className={classes.search} >
+                           
+                          <div className={classes.searchIcon}> 
+                             <Autocomplete 
+                                className="pading"  
+                               
+                                options={this.state.ProductData}  
+                                
+                                getOptionLabel={option => option.name}     
+                                getOptionId={option =>option.id}                            
+                                style={{ width: 200 }}                                      
+                                renderInput={params => (
+                                  <TextField  
+                                    classes={{
+                                      root: classes.inputRoot,
+                                      input: classes.inputInput
+                                            
+                                    }} 
+                                    {...params}
+                                    label="Search..."
+                                    variant="outlined"
+                                    fullWidth 
+                                     />
+
+                                    )}  
+
+                              /> 
+                          </div>
+                                
+                           
+                    </div>  
+
+                )  
+        }  
+  
+}  
+export default withStyles(useStyles)(AComplete)
